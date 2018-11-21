a7aec3a377aee4861febe5ffaaeda4b1417cd5fd
leopuglia-a <leopuglia.a@gmail.com>
2018-11-21 09:56:12 -0200

    Final version

--- main.c
+++ main.c
@@ -7,18 +7,7 @@
 #include "include/LCD.h"
 #include "include/bitop.h"
 
-#define input 0
-#define output 1
-
-
-const char temperatura[] PROGMEM = 	"Temp:\0"; 	//mensagem armazenada na memória flash
-const char umidade[] PROGMEM = 		"Umid:\0"; 
-
 int main(void){	
-	char tem[3], umi[4];
-
-	DDRD = 0xFF; //PORTD como saída
-    DDRB = 0xFF; //PORTB como saída
 
 	inic_LCD_4bits(); //inicializa o LCD
 
@@ -27,26 +16,8 @@ int main(void){
 
 	while(1){
 		sensor_read(&sensor);
-		
-		// transforma os valores lidos do sensor em string
-		itos(sensor.value.temp, tem);
-		itos(sensor.value.hum, umi);
-
-		strcat(tem, "\337C");
-		strcat(umi, "%");
-		
-	    cmd_LCD(0x01,0); 	// limpa display
-	    cmd_LCD(0x80,0); 	// posiciona cursor na primeira posição
-
-		escreve_LCD_Flash(temperatura);
-		
-		cmd_LCD(0x8C,0);	// alinha os valores
-		escreve_LCD(tem);
-
-		cmd_LCD(0xC0,0); 	//desloca cursor para a segunda linha
-		escreve_LCD_Flash(umidade);
-	    
-		cmd_LCD(0xCD,0);	// alinha os valores
+				
+		show_on_LCD(sensor.value.temp,sensor.value.hum);
 
 		_delay_ms(1000);
 	}
